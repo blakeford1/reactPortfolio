c41dd5ce584ddec7550ba7c1cfb3ded86ad4abe7
Blake Ford <blakeaford88@gmail.com>
2020-08-19 10:52:27 -0600

    almost done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Navbar from './components/Nav';
-import Portfolio from './components/Portfolio';
-import Resume from './components/Resume';
-import Contact from './components/Contact';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Navbar from "./components/Nav";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Navbar />
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/portfolio' component={Portfolio} />
-          <Route exact path='/resume' component={Resume} />
-          <Route exact path='/contact' component={Contact} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
         </Switch>
       </div>
     </Router>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav
-      className='navbar bg-primary'
-      style={{ fontSize: '4.2rem', justifyContent: 'center' }}>
+      className="navbar bg-primary"
+      style={{ fontSize: "4.2rem", justifyContent: "center" }}
+    >
       <ul>
         <li>
-          <Link to='/'>Home</Link>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to='/about'>About</Link>
+          <Link to="/about">About</Link>
         </li>
         <li>
-          <Link to='/portfolio'>Portfolio</Link>
+          <Link to="/portfolio">Portfolio</Link>
         </li>
         <li>
-          <Link to='/resume'>Resume</Link>
-        </li>
-        <li>
-          <Link to='/contact'>Contact</Link>
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
     </nav>
