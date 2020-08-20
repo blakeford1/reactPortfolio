b61c69de0aebbf2b88adcb26a0fb6dc3d5cb33b4
Blake Ford <blakeaford88@gmail.com>
2020-08-19 20:52:12 -0600

    this git commit -m progress

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static("build"));
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build/index.html"));
+  res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 app.listen(port, () => {
   console.log("Server is up!");
