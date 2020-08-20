5dd5153940d62623e5c02a320a9ea91052795cfd
Blake Ford <blakeaford88@gmail.com>
2020-08-19 20:20:39 -0600

    Updates

---
+++ precache-manifest.1b1a297be2e69a8beb5a3bce666ac6f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d548f798d45dc149c9a2a0641a6438e2",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "c7a1817cdb91b6b41875",
+    "url": "/reactPortfolio/static/css/main.d4f2e83e.chunk.css"
+  },
+  {
+    "revision": "0227a98869bbe5ad13c3",
+    "url": "/reactPortfolio/static/js/2.95cd203c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactPortfolio/static/js/2.95cd203c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a1817cdb91b6b41875",
+    "url": "/reactPortfolio/static/js/main.ace917c5.chunk.js"
+  },
+  {
+    "revision": "9b0f8995f6f76a603e43",
+    "url": "/reactPortfolio/static/js/runtime-main.2ba24d27.js"
+  }
+]);
